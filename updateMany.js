81b3fb86d7b46541794c5104a26194c11307f401
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-08-31 18:59:28 +0530

    updatedMany

---
+++ updateMany.js
@@ -0,0 +1,26 @@
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var url = 'mongodb://localhost:27017/';
+
+MongoClient.connect(url, function(err, database){
+    if(err){
+        throw err;
+    }
+
+    var dbase = database.db("myDatabase");
+
+    var whereClause = { address: /Harvansh nagar/ };
+    var newValue = { $set: {address: "Gaziabad"} };
+    
+
+    dbase.collection("user").updateMany(
+        whereClause, newValue, 
+        function(error, response){
+        if(error){
+            throw error;
+        }
+
+        console.log("updatedValue:- ", response);
+        database.close();
+    })
+})
